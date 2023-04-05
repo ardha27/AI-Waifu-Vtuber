175615175eb98ad115989990df1075e9d1eee98f
ardha27 <ardhasemaranatha@gmail.com>
2023-04-05 13:03:28 +0700

    detect_google

--- utils/translate.py
+++ utils/translate.py
@@ -34,11 +34,25 @@ def translate_deeplx(text, source, target):
 
     return translated_text
 
-def translate_google(text, target):
+def translate_google(text, source, target):
     try:
         translator = googletrans.Translator()
-        result = translator.translate(text, dest=target)
+        result = translator.translate(text, src=source, dest=target)
         return result.text
     except:
         print("Error translate")
         return
+    
+def detect_google(text):
+    try:
+        translator = googletrans.Translator()
+        result = translator.detect(text)
+        return result.lang.upper()
+    except:
+        print("Error detect")
+        return
+
+if __name__ == "__main__":
+    text = "aku tidak menyukaimu"
+    source = translate_deeplx(text, "ID", "JA")
+    print(source)
